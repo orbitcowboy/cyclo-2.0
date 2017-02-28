5cb7e2e6cc4dbd006d33d6c09ffc9a98c68f4a7e
orbitcowboy <orbitcowboy@web.de>
2017-02-28 11:22:19 +0100

    Added a script to run PVS Studio for Linux.

--- test-input.c
+++ test-input.c
@@ -1,3 +1,5 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 int main(int argc, char *argv[])
 {
 	if (Faking > 0)
